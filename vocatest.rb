f64d28343396f943f96b704631df465dde3f9dfa
Ubuntu <ubuntu@ip-172-31-32-179.ap-northeast-1.compute.internal>
2023-03-01 07:44:38 +0000

    terail

--- vocatest.rb
+++ vocatest.rb
@@ -6,7 +6,10 @@
 
 処理
 Bの単語をAと照合し照合結果に応じて点数を判断する
-
+完全一致２点 answer == student
+単語の綴り＋文字数が違う　０点answer == student && !answer.length == student.length
+単語の綴りが１文字違う　＋文字数はあっている１点 answer.length == student.length
+単語の綴りが２文字以上違う ＋文字数はあっている
 出力
 合計点
 
@@ -15,11 +18,25 @@ Bの単語をAと照合し照合結果に応じて点数を判断する
 N = gets.to_i
 
 answers = []
-students = []
+students= []
 
 N.times do 
 answers.push(gets.to_s) 
 students.push(gets.to_s)
 end
 
-puts answers
+#[["apple","appple"]]
+a = answers.zip(students)
+# ["a","p","p","l","l","e"]
+a.map{|b,c| b.chars.}
+
+# answers.each do |answer|
+# students.each do |student|
+
+
+
+
+
+
+# end
+# end
